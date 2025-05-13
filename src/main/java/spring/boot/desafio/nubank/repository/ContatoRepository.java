641ad6db42634929730139f30e7cd2b8954b8814
PedrodeAndradecf <>
2025-05-13 17:52:50 -0300

    UP erros contrl e requisiçao

--- src/main/java/spring/boot/desafio/nubank/repository/ContatoRepository.java
+++ src/main/java/spring/boot/desafio/nubank/repository/ContatoRepository.java
@@ -2,7 +2,8 @@ package spring.boot.desafio.nubank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.boot.desafio.nubank.model.Clientes;
+import spring.boot.desafio.nubank.model.Contato;
 
-public interface ContatoRepository extends JpaRepository<Clientes,Long> {
+public interface ContatoRepository extends JpaRepository<Contato,Long> {
     
 }
